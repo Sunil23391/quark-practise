d577bfdc35711696a9732c4659264c98b927c40d
Sunil Choudhary <sunil.sunxiil.choudhary@gmail.com>
2020-11-07 12:30:08 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa645a979530b001fd1e424",
+				"5fa645a979530b001fd1e426"
+			]
+		},
+		"5fa645a979530b001fd1e424": {
+			"id": "5fa645a979530b001fd1e424",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa645a979530b001fd1e426": {
+			"id": "5fa645a979530b001fd1e426",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
